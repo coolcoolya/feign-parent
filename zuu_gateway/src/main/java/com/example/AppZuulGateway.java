d90372fbb0a9c88a8298d820f541e9e5af5c1aaf
wangya <731716125@qq.com>
2020-02-24 16:22:19 +0800

    使用git去管理网关

--- zuu_gateway/src/main/java/com/example/AppZuulGateway.java
+++ zuu_gateway/src/main/java/com/example/AppZuulGateway.java
@@ -2,8 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @EnableZuulProxy//开启网关代理
 @EnableEurekaClient
@@ -11,5 +14,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class AppZuulGateway {
     public static void main(String[] args) {
         SpringApplication.run(AppZuulGateway.class,args);
+    }
+
+    //zuul配置能够实时更新
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
     }
 }
